8f96e9f39ad91d0f352fc2990a8cd65b3311c241
Eddy Muzaffar <eddy.muzaffar@outlook.com>
2021-12-24 12:18:48 +0100

    Add files via upload
    Ajout du projet Biblio (Ajouter des livre selon l'auteur, la date de parution, la rangée, ...)

---
+++ biblio/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.demo2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+    opens com.example.biblio to javafx.fxml;
+    opens com.example.biblio.model;
+    exports com.example.biblio;
+
+}
